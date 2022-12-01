d1d79864fa81fb35875fda43779902dd8a81b0bb
alex-pakin <aleksei_pakin@mail.ru>
2022-12-01 23:06:27 +0300

    allure settings

--- src/test/java/ru/netology/delivery/test/DeliveryFormTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryFormTest.java
@@ -1,6 +1,10 @@
 package ru.netology.delivery.test;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
@@ -18,6 +22,16 @@ public class DeliveryFormTest {
     @BeforeEach
     void openBrowser() {
         open("http://localhost:9999");
+    }
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
     }
 
     @Test
